0a6ec264346ca4ca1adbef4684e62454e741f9e3
aochernov <a.o.chernov@mail.ru>
2015-10-15 23:19:24 +0300

    floatbits1.correct

--- floatbits1.cpp
+++ floatbits1.cpp
@@ -10,39 +10,16 @@ union floatbits1
 int main()
 {
     float input;
-    int i, myint, mantissa[23], exp[8];
+    int i, myint, mant, exp, sign;
     union floatbits1 convert;
     scanf("%f", &input);
     convert.float_float = input;
     myint = convert.float_int;
-    if (myint < 0)
-    {
-        printf("Знак: 1\n");
-        myint = myint & ~(1 << 31);
-    }
-    else
-    {
-        printf("Знак: 0\n");
-    };
-    for (i = 22; i >= 0; i--)
-    {
-        mantissa[i] = myint % 2;
-        myint = myint / 2;
-    };
-    for (i = 7; i >= 0; i--)
-    {
-        exp[i] = myint % 2;
-        myint = myint / 2;
-    };
-    printf("Экспонента: ");
-    for (i = 0; i < 8; i++)
-    {
-        printf("%d", exp[i]);
-    }
-    printf("\nМантисса: ");
-    for (i = 0; i < 23; i++)
-    {
-        printf("%d", mantissa[i]);
-    }
+    mant = myint & ~(511 << 31);
+    exp = myint >> 23;
+    exp = exp & 255;
+    sign = myint >> 31;
+    sign = sign & 1;
+    printf("f = (-1)^%d * 1.%d * 2^(%d-127)", sign, mant, exp);
     return 0;
 }
