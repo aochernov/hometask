3c30e922953bbb3840e28508bbbe2ffd6aa2d7ca
aochernov <a.o.chernov@mail.ru>
2015-10-16 11:45:36 +0300

    bitxor.spaces

--- bitxor.cpp
+++ bitxor.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int bitXor(int x, int y)
 {
 	int z, a, b, t;
-	a=~x;
-	b=~y;
-	z=x&y;
-	t=a&b;
-	z=~z;
-	t=~t;
-	z=z&t;
-	return(z);
+	a = ~x;
+	b = ~y;
+	z = x & y;
+	t = a & b;
+	z = ~z;
+	t = ~t;
+	z = z & t;
+	return (z);
 }
 int main()
 {
