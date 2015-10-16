6ae96b2905fb2fcd567e297f38113ac12745763a
aochernov <a.o.chernov@mail.ru>
2015-10-16 11:46:21 +0300

    fitsbits.spaces

--- fitsbits.cpp
+++ fitsbits.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 int fitsBits(int x, int y)
 {
-    int p=1, z, q, n;
-    p=~p;
-    p=p<<(y+(~0)+(~0));
-    z=x&p;
-    z=z>>(y+(~0));
-    q=~z;
-    q=!q;
-    z=!z;
-    n=q|z;
+    int p = 1, z, q, n;
+    p = ~p;
+    p = p << (y + (~0) + (~0));
+    z = x & p;
+    z = z >> (y + (~0));
+    q = ~z;
+    q = !q;
+    z = !z;
+    n = q | z;
     return (n);
 }
 int main()
