e5dd8cc4af2d6514d30a867ea359ca9d3572c5aa
aochernov <a.o.chernov@mail.ru>
2015-10-16 11:45:07 +0300

    bang.spaces

--- bang.cpp
+++ bang.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int bang(int x)
 {
     int y, z;
-    y=~x;
-    z=y+1;
-    z=~z;
-    y=y&z;
-    y=y>>31;
-    y=y&1;
-    return(y);
+    y = ~x;
+    z = y + 1;
+    z = ~z;
+    y = y & z;
+    y = y >> 31;
+    y = y & 1;
+    return (y);
 }
 int main()
 {
