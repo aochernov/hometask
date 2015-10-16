923cf1f2049fdcb2e46e3b4510e4f6b197d06071
aochernov <a.o.chernov@mail.ru>
2015-10-16 11:47:16 +0300

    ispower.spaces

--- ispower.cpp
+++ ispower.cpp
@@ -3,18 +3,18 @@
 using namespace std;
 int isPower2(int x)
 {
-    int c=1, z, y, d;
-    d=!x;
-    d=!d;
-    c=c<<31;
-    c=c&x;
-    c=!c;
-    y=x+(~0);
-    z=x&y;
-    z=!z;
-    z=z&c;
-    z=z&d;
-    return(z);
+    int c = 1, z, y, d;
+    d = !x;
+    d = !d;
+    c = c << 31;
+    c = c & x;
+    c = !c;
+    y = x + (~0);
+    z = x & y;
+    z = !z;
+    z = z & c;
+    z = z & d;
+    return (z);
 }
 
 int main()
