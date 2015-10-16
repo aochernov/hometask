d18dd185ee24939142f8142e8aefb3bb0edf171d
aochernov <a.o.chernov@mail.ru>
2015-10-16 11:47:30 +0300

    sign.spaces

--- sign.cpp
+++ sign.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 int sign(int x)
 {
-	int a=1, b, z;
-	a=a<<31;
-	b=a&x;
-	z=!x;
-	z=!z;
-	b=b>>31;
-	z=z|b;
-	return(z);
+	int a = 1, b, z;
+	a = a << 31;
+	b = a & x;
+	z = !x;
+	z = !z;
+	b = b >> 31;
+	z = z | b;
+	return (z);
 }
 int main()
 {
