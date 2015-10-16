51583a7c0c68f02680a0faced52a657e725f9982
aochernov <a.o.chernov@mail.ru>
2015-10-16 11:45:55 +0300

    conditional.spaces

--- conditional.cpp
+++ conditional.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int conditional(int x, int y, int z)
 {
     int p, q, n;
-    p=!x;
-    p=p<<31;
-    p=p>>31;
-    q=~p;
-    p=p&z;
-    q=q&y;
-    n=q|p;
-    return(n);
+    p = !x;
+    p = p << 31;
+    p = p >> 31;
+    q = ~p;
+    p = p & z;
+    q = q & y;
+    n = q | p;
+    return (n);
 }
 int main()
 {
