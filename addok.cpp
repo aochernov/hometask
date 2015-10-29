651b05aac69d627b2b9d20369187aa61970a5ddf
aochernov <a.o.chernov@mail.ru>
2015-10-29 19:39:54 +0300

    addok.3

--- addok.cpp
+++ addok.cpp
@@ -3,21 +3,16 @@
 using namespace std;
 int addOK(int x, int y)
 {
-	int a = 1, n = 1, x1, y1, x2, y2, z, q, c;
-	c = a << 31;
-	c = ~c;
-	x1 = c & x;
-	y1 = c & y;
-	z = x1 + y1;
-	z = z >> 31;
-	z = z & n;
-	x2 = x >> 31;
-	y2 = y >> 31;
-	x2 = x2 & n;
-	y2 = y2 & n;
-	q = x2 | y2;
-	q = z & q;
-	return(q);
+	int a, x1, y1, b;
+    a = x + y;
+    x1 = x >> 31;
+    y1 = y >> 31;
+    b = a >> 31;
+    x1 = x1 ^ b;
+    y1 = y1 ^ b;
+    a = x1 & y1;
+    a = !a;
+    return (a);
 }
 int main()
 {
