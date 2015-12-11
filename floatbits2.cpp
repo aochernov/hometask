0ac32ff73e897c9a5ac4f09e021e2258338d1d36
aochernov <a.o.chernov@mail.ru>
2015-12-11 11:19:09 +0300

    Update floatbits2.cpp

--- floatbits2.cpp
+++ floatbits2.cpp
@@ -13,14 +13,36 @@ union converter
     float struct_float;
     struct floatbits2 struct_struct;
 };
+
+int printfloat (int sign, int mant, int exp)
+{
+    if (exp == 255 && mant != 0)
+    {
+        printf("NaN");
+    }
+    if (exp == 255 && mant == 0 && sign == 1)
+    {
+        printf("-inf");
+    }
+    if (exp == 255 && mant == 0 && sign == 0)
+    {
+        printf("+inf");
+    }
+    else
+    {
+        printf("f = (-1)^%d * 1.%d * 2^(%d-127)", sign, mant, exp);
+    }
+}
+
 int main()
 {
     float input;
     struct floatbits2 convert;
     union converter myconverter;
-    scanf("%f", &input);
+    sscanf("%f %f", &input, &input1);
+    input = input/input1;
     myconverter.struct_float = input;
     convert = myconverter.struct_struct;
-    printf("f = (-1)^%d * 1.%d * 2^(%d-127)", convert.sign, convert.mant, convert.exp);
+    printfloat(convert.sign, convert.mant, convert.exp);
     return 0;
 }
