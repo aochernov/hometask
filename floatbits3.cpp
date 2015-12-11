8e737e05aea1b7614c9a119623fa9175bc7abd2f
aochernov <a.o.chernov@mail.ru>
2015-12-11 11:19:22 +0300

    Update floatbits3.cpp

--- floatbits3.cpp
+++ floatbits3.cpp
@@ -3,10 +3,31 @@
 
 using namespace std;
 
+int printfloat (int sign, int mant, int exp)
+{
+    if (exp == 255 && mant != 0)
+    {
+        printf("NaN");
+    }
+    if (exp == 255 && mant == 0 && sign == 1)
+    {
+        printf("-inf");
+    }
+    if (exp == 255 && mant == 0 && sign == 0)
+    {
+        printf("+inf");
+    }
+    else
+    {
+        printf("f = (-1)^%d * 1.%d * 2^(%d-127)", sign, mant, exp);
+    }
+}
+
 int main()
 {
     float input;
-    scanf("%d", &input);
+    scanf("%f %f", &input, &input1);
+    input = input/input1;
     int sign, exp = 0, mantissa = 0;
     int* input_pointer;
     input_pointer = (int*)&input;
@@ -18,6 +39,6 @@ int main()
     exp = exp >> 23;
     mantissa = *input_pointer;
     mantissa = mantissa & ~(511 << 31);
-    printf("f = (-1)^%d * 1.%d * 2^(%d-127)", sign, mantissa, exp);
+    printfloat(sign, mantissa, exp);
     return 0;
 }
